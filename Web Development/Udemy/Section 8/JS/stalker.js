b26fee4e52a7a2e6fd86c203f237edf975e2335a
DGinder <darrin.ginder@yahoo.com>
2019-03-03 13:54:50 -0800

    3/3/2019

---
+++ Web Development/Udemy/Section 8/JS/stalker.js
@@ -0,0 +1,7 @@
+var fName = prompt("What is your first name");
+var lName = prompt("What is your last name");
+var age = prompt("What is your age");
+
+console.log("It's nice to eat you " + fName + " " + lName);
+console.log("It'll be great to taste a " + age + " year old cuisine");
+
