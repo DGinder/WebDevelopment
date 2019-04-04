930b613903ca669e32abfe009a622a5dcab7bcaa
DGinder <darrin.ginder@yahoo.com>
2019-04-03 20:35:38 -0700

    4-3-2019

--- Web Development/Udemy/Section 11/ToDoList/Assets/JS/toDoJs.js
+++ Web Development/Udemy/Section 11/ToDoList/Assets/JS/toDoJs.js
@@ -1,13 +1,31 @@
 /// <reference types="jquery" />
 //Check Off Specific Items By Clicking
-$("li").on("click", function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("done")
 })
 
 //click x equals delete
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     })
     event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function (event) {
+    if(event.which === 13){
+        //get text
+        toDoText = $(this).val();
+
+        //create new li
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + toDoText + "</li>");
+
+        //clear input
+        $(this).val("")
+
+    }
+})
+
+$(".fa-plus").click(function() {
+    $("input[type='text']").slideToggle();
 })
