f87f0b18341be3d77cc00530e45db8949e10af65
DGinder <darrin.ginder@yahoo.com>
2019-07-14 22:58:39 -0700

    7/14/19

--- Web Development/Udemy/YelpCamp/app.js
+++ Web Development/Udemy/YelpCamp/app.js
@@ -1,9 +1,17 @@
 var express = require("express"),
 app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 var request = require("request");
 app.set("view engine", "ejs");
 
 var temp = [
+    {name:"Salmon Creek", image: "https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"Yellowstone", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"Other", image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"Salmon Creek", image: "https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"Yellowstone", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"Other", image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
     {name:"Salmon Creek", image: "https://images.unsplash.com/photo-1440262206549-8fe2c3b8bf8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
     {name:"Yellowstone", image: "https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
     {name:"Other", image: "https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"}
@@ -15,6 +23,21 @@ app.get("/", (req, res) => {
 
 app.get("/campgrounds", (req, res) => {
     res.render("campgrounds", {data: temp});
+})
+
+
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new.ejs");
+})
+
+app.post("/campgrounds", (req, res) => {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    temp.push(newCampground);
+
+    res.redirect("/campgrounds");
 })
 
 
