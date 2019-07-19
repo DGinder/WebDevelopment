933734414f6a2d024e47031b0abb4390b1a55cdf
DGinder <darrin.ginder@yahoo.com>
2019-07-18 20:31:34 -0700

    7/18/2019

---
+++ Web Development/Udemy/Section 14/CatApp/cat.js
@@ -0,0 +1,51 @@
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/cat_app", { useNewUrlParser: true });
+
+var catSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    temperament: String
+});
+
+var Cat = mongoose.model("Cat", catSchema);
+
+
+
+//add to data base with new and save
+// var george = new Cat({
+//     name: "Ms. Norris",
+//     age: 11,
+//     temperament: "Evil"
+// });
+// george.save((err, cat) => {
+//     if(err){
+//         console.log("Something went wrong")
+//     }
+//     else{
+//         console.log("Saved" + cat)
+//     }
+// });
+
+//add cat with create
+Cat.create({
+    name: "Snow Whtie",
+    age: 15,
+    temperament: "Bland"
+}, (err, cat) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log(cat)
+    }
+})
+
+// Cat.find({}, (err, cats) => {
+//     if(err){
+//         console.log("Oh No Error");
+//         console.log(err)
+//     }else{
+//         console.log((cats));
+//     }
+// })
+
